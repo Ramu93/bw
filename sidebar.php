6c604383ec7162c505b15b5a3420cbc4b98fb1e3
Ramu Ramasamy <rram.ramasamy@gmail.com>
2017-07-07 08:17:24 +0530

    sidebar
    expand all 2 level menus in "master" menu

--- sidebar.php
+++ sidebar.php
@@ -27,7 +27,7 @@
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
-              <ul class="treeview-menu">
+              <ul class="treeview-menu" style="display:block;">
                 <li><a href="<?php echo HOMEURL; ?>/master/item-master-create.php"><i class="fa fa-plus"></i> Create</a></li>
                 <li><a href="<?php echo HOMEURL; ?>/master/item-master-view.php"><i class="fa fa-file"></i> View</a></li>
               </ul>
@@ -38,7 +38,7 @@
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
-              <ul class="treeview-menu">
+              <ul class="treeview-menu" style="display:block;">
                 <li><a href="<?php echo HOMEURL; ?>/master/party-master-create.php"><i class="fa fa-plus"></i> Create</a></li>
                 <li><a href="<?php echo HOMEURL; ?>/master/party-master-view.php"><i class="fa fa-file"></i> View</a></li>
               </ul>
@@ -49,7 +49,7 @@
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
-              <ul class="treeview-menu">
+              <ul class="treeview-menu" style="display:block;">
                 <li><a href="<?php echo HOMEURL; ?>/master/employee-master-create.php"><i class="fa fa-plus"></i> Create</a></li>
                 <li><a href="<?php echo HOMEURL; ?>/master/employee-master-view.php"><i class="fa fa-file"></i> View</a></li>
               </ul>
@@ -60,7 +60,7 @@
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
-              <ul class="treeview-menu">
+              <ul class="treeview-menu" style="display:block;">
                 <li><a href="<?php echo HOMEURL; ?>/master/role-master-create.php"><i class="fa fa-plus"></i> Create</a></li>
                 <li><a href="<?php echo HOMEURL; ?>/master/role-master-view.php"><i class="fa fa-file"></i> View</a></li>
               </ul>
@@ -71,7 +71,7 @@
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
-              <ul class="treeview-menu">
+              <ul class="treeview-menu" style="display:block;">
                 <li><a href="<?php echo HOMEURL; ?>/master/tarrif-master-create.php"><i class="fa fa-plus"></i> Create</a></li>
                 <li><a href="<?php echo HOMEURL; ?>/master/tarrif-master-view.php"><i class="fa fa-file"></i> View</a></li>
               </ul>
