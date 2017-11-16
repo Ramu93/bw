5ee9a3ee12a8aab68e2fb312d49833e2673e5ee5
Ramu Ramasamy <rram.ramasamy@gmail.com>
2017-11-17 01:57:06 +0530

    general-grn-filtering

--- general/grn/grn-list-view.php
+++ general/grn/grn-list-view.php
@@ -2,6 +2,17 @@
   include('../header.php');
   include('../sidebar.php');
   include('../dbconfig.php');
+
+  if(isset($_GET['filter_from'])){
+    $filterFrom = $_GET['filter_from'];
+  } else {
+    $filterFrom = date("Y-m-d");
+  }
+  if(isset($_GET['filter_to'])){
+    $filterTo = $_GET['filter_to'];
+  } else {
+    $filterTo = date("Y-m-d");
+  }
 ?>
 
   <!-- Content Wrapper. Contains page content -->
@@ -19,10 +30,28 @@
       <!-- Default box -->
       <div class="box">
         <div class="box-body">
+          <form onsubmit="return false;" method="">
+            <div class="row">
+              <div class="col-md-3 col-sm-3">
+                <label for="from_date_label">From date</label>
+                <input type="text" tabindex="" class="form-control" id="filter_from" name="filter_from" placeholder="" value="">
+              </div>
+              <div class="col-md-3 col-sm-3">
+                <label for="to_date_label">To date</label>
+                <input type="text" tabindex="" class="form-control" id="filter_to" name="filter_to" placeholder="" value="">
+              </div>
+              <div class="col-md-3 col-sm-3">
+                &nbsp;
+                <input type="button" name="select_items" value="Go" class="btn btn-primary btn-block pull-left" onclick="loadPage()">
+              </div>
+            </div>
+          </form>
+            &nbsp;
           <table id="grn_table" class="table table-bordered table-striped">
             <thead>
               <tr>
                 <th>S. No.</th>
+                <th>Created Date</th>
                 <th>GRN ID</th>
                 <th>PAR ID</th>
                 <th>Action</th>
@@ -30,7 +59,7 @@
             </thead>
             <tbody>
               <?php 
-                $select_query = "SELECT * FROM general_good_receipt_note";
+                $select_query = "SELECT * FROM general_good_receipt_note WHERE created_date BETWEEN '$filterFrom' AND '$filterTo'";
                 $result = mysqli_query($dbc,$select_query);
                 $row_counter = 0;
                 if(mysqli_num_rows($result) > 0) {
@@ -38,6 +67,7 @@
                   while($row = mysqli_fetch_array($result)) {
                     echo "<tr>";
                     echo "<td>".++$row_counter."</td>";
+                    echo "<td>".$row['created_date']."</td>";
                     echo "<td>".$row['grn_id']."</td>";
                     echo "<td>".$row['par_id']."</td>";
                     echo "<td><a href='grn-view.php?grn_id=".$row['grn_id']."'>View</a></td>";
@@ -70,4 +100,43 @@
         $("#grn_table").DataTable();
       <?php } ?>
     });
+
+    $(document).ready(function(){
+      $('#filter_from').val(getCurrentDate());
+      $('#filter_to').val(getCurrentDate());
+    });
+
+    $('#filter_from').datepicker({
+      autoclose: true,
+      dateFormat: "yy-mm-dd",
+    });
+    $('#filter_to').datepicker({
+      autoclose: true,
+      dateFormat: "yy-mm-dd",
+    });
+
+    function getCurrentDate(){
+      var today = new Date();
+      var yy = today.getFullYear();
+      var mm = today.getMonth();
+      var dd = today.getDate();
+      mm += 1;
+      if(dd < 10){
+        dd = '0' + dd;
+      } 
+      if(mm < 10){
+        mm = '0' + mm;
+      }
+      var todayDate = yy + '-' + mm + '-' + dd;
+      return todayDate;
+    }
+
+
+    function loadPage(){
+      var filterFrom = $('#filter_from').val();
+      var filterTo = $('#filter_to').val();
+      var url = 'grn-list-view.php?';
+      url += '&filter_from=' + filterFrom + '&filter_to=' + filterTo;
+      this.document.location.href = url;
+    }
   </script>
