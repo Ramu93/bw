8aef119efff01dc3c6109d49319593234ea3a67f
Ramu Ramasamy <rram.ramasamy@gmail.com>
2017-11-17 00:25:56 +0530

    bonded - igp - loading - filtering

--- bonded/gate-pass/igp-loading-list-view.php
+++ bonded/gate-pass/igp-loading-list-view.php
@@ -2,6 +2,9 @@
   include('../header.php');
   include('../sidebar.php');
   include('../dbconfig.php');
+
+  $filterFrom = $_GET['filter_from'];
+  $filterTo = $_GET['filter_to'];
 ?>
 
   <!-- Content Wrapper. Contains page content -->
@@ -19,10 +22,28 @@
       <!-- Default box -->
       <div class="box">
         <div class="box-body">
+          <form onsubmit="return false;" method="">
+            <div class="row">
+              <div class="col-md-3 col-sm-3">
+                <label for="from_date_label">From date</label>
+                <input type="text" tabindex="" class="form-control" id="filter_from" name="filter_from" placeholder="" value="">
+              </div>
+              <div class="col-md-3 col-sm-3">
+                <label for="to_date_label">To date</label>
+                <input type="text" tabindex="" class="form-control" id="filter_to" name="filter_to" placeholder="" value="">
+              </div>
+              <div class="col-md-3 col-sm-3">
+                &nbsp;
+                <input type="button" name="select_items" value="Go" class="btn btn-primary btn-block pull-left" onclick="loadPage()">
+              </div>
+            </div>
+          </form>
+            &nbsp;
           <table id="par_table" class="table table-bordered table-striped">
             <thead>
               <tr>
                 <th>S. No.</th>
+                <th>Created Date</th>
                 <th>IGP ID</th>
                 <th>Vehicle Number</th>
                 <th>Driver Name</th>
@@ -31,7 +52,7 @@
             </thead>
             <tbody>
               <?php 
-                $select_query = "SELECT * FROM bonded_igp_loading";
+                $select_query = "SELECT * FROM bonded_igp_loading WHERE created_date BETWEEN '$filterFrom' AND '$filterTo'";
                 $result = mysqli_query($dbc,$select_query);
                 $row_counter = 0;
                 if(mysqli_num_rows($result) > 0) {
@@ -40,6 +61,7 @@
                     echo "<tr>";
                     echo "<td>".++$row_counter."</td>";
                     echo "<td>".$row['igp_lo_id']."</td>";
+                    echo "<td>".$row['created_date']."</td>";
                     echo "<td>".$row['vehicle_number']."</td>";
                     echo "<td>".$row['driver_name']."</td>";
                     echo "<td><a href='igp-loading-view.php?igp_lo_id=".$row['igp_lo_id']."'>View</a></td>";
@@ -72,4 +94,42 @@
         $("#par_table").DataTable();
       <?php } ?>
     });
+    $(document).ready(function(){
+      $('#filter_from').val(getCurrentDate());
+      $('#filter_to').val(getCurrentDate());
+    });
+
+    $('#filter_from').datepicker({
+      autoclose: true,
+      dateFormat: "yy-mm-dd",
+    });
+    $('#filter_to').datepicker({
+      autoclose: true,
+      dateFormat: "yy-mm-dd",
+    });
+
+    function getCurrentDate(){
+      var today = new Date();
+      var yy = today.getFullYear();
+      var mm = today.getMonth();
+      var dd = today.getDate();
+      mm += 1;
+      if(dd < 10){
+        dd = '0' + dd;
+      } 
+      if(mm < 10){
+        mm = '0' + mm;
+      }
+      var todayDate = yy + '-' + mm + '-' + dd;
+      return todayDate;
+    }
+
+
+    function loadPage(){
+      var filterFrom = $('#filter_from').val();
+      var filterTo = $('#filter_to').val();
+      var url = 'igp-loading-list-view.php?';
+      url += '&filter_from=' + filterFrom + '&filter_to=' + filterTo;
+      this.document.location.href = url;
+    }
   </script>
